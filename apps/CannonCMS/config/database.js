6aee13aa1c090c84a1dfa31f564c4da26bfa36bf
James <dimonaco.james@gmail.com>
2022-11-15 15:00:53 +0000

    database config updates

--- apps/CannonCMS/config/database.js
+++ apps/CannonCMS/config/database.js
@@ -1,27 +1,11 @@
-module.exports = ({ env }) => ({
-    connection: {
-      client: "postgres",
-      connection: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
-      },
-      useNullAsDefault: true,
-      debug: false,
-      acquireConnectionTimeout: 600000,
-      pool: {
-        min: 0,
-        max: 100,
-        acquireTimeoutMillis: 300000,
-        createTimeoutMillis: 300000,
-        destroyTimeoutMillis: 50000,
-        idleTimeoutMillis: 300000,
-        reapIntervalMillis: 10000,
-        createRetryIntervalMillis: 2000,
-        propagateCreateError: false,
-      },
-    },
+const path = require('path');
 
-  });
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
+});
