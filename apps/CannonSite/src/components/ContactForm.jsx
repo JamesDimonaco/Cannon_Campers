76812e98e6a30ba893ac9a5255b160aa9eddea08
James Dimonaco <james@whereyouat.co.uk>
2023-02-20 10:54:12 +0000

    update contact form

--- apps/CannonSite/src/components/ContactForm.jsx
+++ apps/CannonSite/src/components/ContactForm.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react'
 import poster from '../../lib/poster'
 import Toast from './Toast'
-import { Bars3Icon, EnvelopeIcon, PhoneIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ClockIcon, EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 
 export function ContactForm() {
   const [formData, setFormData] = useState({
@@ -66,6 +66,12 @@ export function ContactForm() {
         setShowToast(true)
       }
     }
+
+
+    setTimeout(() => {
+      setShowToast(false)
+    }, 3500)
+    
 
     setIsSubmitting(false)
   }
@@ -190,44 +196,18 @@ export function ContactForm() {
       </form>
       </div> */}
 
-      <hr/>
 
-      <section className="py-28 relative bg-white" aria-labelledby="contact-heading">
+      <section className="py-32 relative bg-white" aria-labelledby="contact-heading">
           <div className="absolute h-1/2 w-full bg-warm-gray-50" aria-hidden="true" />
-          {/* Decorative dot pattern */}
-          <div className="relative mx-auto max-w-7xl px-6 lg:px-8">
-            <svg
-              className="absolute top-0 right-0 z-0 -translate-y-16 translate-x-1/2 transform sm:translate-x-1/4 md:-translate-y-24 lg:-translate-y-72"
-              width={404}
-              height={384}
-              fill="none"
-              viewBox="0 0 404 384"
-              aria-hidden="true"
-            >
-              <defs>
-                <pattern
-                  id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect x={0} y={0} width={4} height={4} className="text-warm-gray-200" fill="currentColor" />
-                </pattern>
-              </defs>
-              <rect width={404} height={384} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
-            </svg>
-          </div>
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="relative bg-white shadow-xl">
               <h2 id="contact-heading" className="sr-only">
                 Contact us
               </h2>
 
-              <div className="grid grid-cols-1 lg:grid-cols-3">
+              <div className="grid grid-cols-1 lg:grid-cols-3 overflow-hidden">
                 {/* Contact information */}
-                <div className="relative overflow-hidden bg-gradient-to-b from-teal-500 to-teal-600 py-10 px-6 sm:px-10 xl:p-12">
+                <div className="relative bg-gradient-to-b from-teal-500 to-teal-600 py-10 px-6 sm:px-8 xl:p-12">
                   {/* Decorative angle backgrounds */}
                   <div className="pointer-events-none absolute inset-0 sm:hidden" aria-hidden="true">
                     <svg
@@ -324,8 +304,7 @@ export function ContactForm() {
                   </div>
                   <h3 className="text-lg font-medium text-white">Contact information</h3>
                   <p className="mt-6 max-w-3xl text-base text-teal-50">
-                    Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor
-                    lacus arcu.
+                  FROM UPGRADES TO FULL CONVERSIONS GET IN TOUCH TODAY!
                   </p>
                   <dl className="mt-8 space-y-6">
                     <dt>
@@ -333,19 +312,27 @@ export function ContactForm() {
                     </dt>
                     <dd className="flex text-base text-teal-50">
                       <PhoneIcon className="h-6 w-6 flex-shrink-0 text-teal-200" aria-hidden="true" />
-                      <span className="ml-3">+1 (555) 123-4567</span>
+                      <span className="ml-3">01873 811118</span>
                     </dd>
                     <dt>
                       <span className="sr-only">Email</span>
                     </dt>
                     <dd className="flex text-base text-teal-50">
                       <EnvelopeIcon className="h-6 w-6 flex-shrink-0 text-teal-200" aria-hidden="true" />
-                      <span className="ml-3">support@workcation.com</span>
+                      <span className="ml-3 lg:text-sm">contact@cannoncamperconversions.com</span>
                     </dd>
+                    <dt>
+                    <span className="sr-only">Opening times</span>
+                    </dt>
+                    <dd className="flex text-base text-teal-50">
+                      <ClockIcon className="h-6 w-6 flex-shrink-0 text-teal-200" aria-hidden="true" />
+                      <span className="ml-3 lg:text-sm"> Mon - Fri 9:00am to 4.30pm Sat Appointment Only</span>
+                    </dd>
+                    
                   </dl>
                   <ul role="list" className="mt-8 flex space-x-12">
                     <li>
-                      <a className="text-teal-200 hover:text-teal-100" href="#">
+                      <a className="text-teal-200 hover:text-teal-100" target={'_blank'} href="https://www.facebook.com/T6.Cannon.camper.conversioins/">
                         <span className="sr-only">Facebook</span>
                         <svg className="h-7 w-7" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
                           <path
@@ -357,7 +344,7 @@ export function ContactForm() {
                       </a>
                     </li>
                     <li>
-                      <a className="text-teal-200 hover:text-teal-100" href="#">
+                      <a className="text-teal-200 hover:text-teal-100" target={'_blank'} href="#">
                         <span className="sr-only">GitHub</span>
                         <svg className="h-7 w-7" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
                           <path
@@ -369,7 +356,7 @@ export function ContactForm() {
                       </a>
                     </li>
                     <li>
-                      <a className="text-teal-200 hover:text-teal-100" href="#">
+                      <a className="text-teal-200 hover:text-teal-100" target={'_blank'} href="https://twitter.com/cannoncampers">
                         <span className="sr-only">Twitter</span>
                         <svg className="h-7 w-7" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
                           <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -394,7 +381,7 @@ export function ContactForm() {
                           name="firstName"
                           id="firstName"
                           autoComplete="given-name"
-                          className="block w-full rounded-md border-warm-gray-300 py-3 px-4 text-warm-gray-900 shadow-sm focus:border-teal-500 focus:ring-teal-500"
+                          className={`block w-full rounded-md border-warm-gray-300 py-3 px-4 text-warm-gray-900 shadow-sm focus:border-teal-500 focus:ring-teal-500 ${formErrors.name ? 'border-red-500' : ''}`}
                         />
                       </div>
                     </div>
@@ -433,9 +420,6 @@ export function ContactForm() {
                         <label htmlFor="phone" className="block text-sm font-medium text-warm-gray-900">
                           Phone
                         </label>
-                        <span id="phone-optional" className="text-sm text-warm-gray-500">
-                          Optional
-                        </span>
                       </div>
                       <div className="mt-1">
                         <input
